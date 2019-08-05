88cd4ff572fcaabf567ed1b71ff9e4a3b1e66a9a
GaurangTandon <1gaurangtandon@gmail.com>
2019-08-05 22:05:42 +0530

    making it cleaner

--- A1/q2.c
+++ A1/q2.c
@@ -7,19 +7,31 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define fileNameSizeLimit 100
+#define fileNameSizeLimit 400
 char newFilePath[fileNameSizeLimit], oldFilePath[fileNameSizeLimit],
     directoryPath[fileNameSizeLimit];
 
+// read file name and return its length
+int readFileName(char path[]) {
+    read(0, path, fileNameSizeLimit);
+    int len = 0;
+    while (path[len])
+        len++;
+    while (len >= 1 && (path[len - 1] == 3 || path[len - 1] == '\n'))
+        len--;
+    path[len] = 0;
+    return len;
+}
+
 int main() {
-    read(0, newFilePath, fileNameSizeLimit);
-    // read(0, oldFilePath, fileNameSizeLimit);
-    // read(0, directoryPath, fileNameSizeLimit);
+    int f1 = readFileName(newFilePath), f2 = readFileName(oldFilePath),
+        f3 = readFileName(directoryPath);
 
     struct stat s;
     write(1, "Directory is created: ", 23);
     if (stat(directoryPath, &s) == -1) {
         write(1, "No\n", 4);
+        perror("Error");
     } else {
         write(1, "Yes\n", 5);
     }
