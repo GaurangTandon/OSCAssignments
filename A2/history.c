8f4b98224554b62d54bf7457c96d64c208aa91fc
GaurangTandon <1gaurangtandon@gmail.com>
2019-08-18 15:45:02 +0530

    added history command

---
+++ A2/history.c
@@ -0,0 +1,41 @@
+#include "history.h"
+#include <stdio.h>
+#include <string.h>
+
+#define max(x, y) (x) >= (y) ? (x) : (y)
+#define maxStored 20
+
+char* commandHistory[maxStored];
+int storedCount = 0;
+void addNewCommand(char* cmd) {
+    if (storedCount > 0 && strcmp(commandHistory[storedCount - 1], cmd) == 0) {
+        return;
+    }
+
+    if (storedCount < maxStored) {
+        commandHistory[storedCount++] = cmd;
+    } else {
+        for (int i = 0; i < maxStored - 1; i++) {
+            commandHistory[i] = commandHistory[i + 1];
+        }
+        commandHistory[maxStored - 1] = cmd;
+    }
+}
+
+void printHistory(int n) {
+    for (int i = storedCount - 1; i >= max(storedCount - n, 0); i--) {
+        printf("%s\n", commandHistory[i]);
+    }
+}
+
+void historyDisplay(int n) {
+    char buf[100] = "history ";
+    sprintf(buf + 8, "%d", n);
+    addNewCommand(buf);
+    printHistory(n);
+}
+
+void historyDisplay() {
+    addNewCommand("history");
+    printHistory(10);
+}
---
+++ A2/history.h
@@ -0,0 +1,3 @@
+char* commandHistory[20];
+int storedCount;
+void addNewCommand(char*);
